6ab19ac72c5aa5deeedb448c85038ba7ba201924
Suraj Patil <srpatil@uci.edu>
2021-02-09 19:50:15 +0530

    Add file

---
+++ hello.scala
@@ -0,0 +1,9 @@
+
+// The scala command will execute a source script by wrapping it in a template and then compiling and executing the resulting program.
+// In interactive mode, the REPL reads expressions at the prompt, wraps them in an executable template, and then compiles and executes 
+// the result.
+
+println("Hello, World")
+
+// So running this file with :load will simply give us the greeting, but running this file in
+// :paste mode will give the following error (error: expected class or object definition)
